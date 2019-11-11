b6a0cec9f7bb0405c572ad0c91b4eefc0cac2462
Isabelle Nguyen <isabelln@usc.edu>
2019-11-11 13:16:47 -0800

    created function for historical

--- newstuff/isabelle/test.py
+++ newstuff/isabelle/test.py
@@ -1,24 +1,42 @@
 import tweepy
 import datetime
 import sys
+from datetime import datetime, date,timedelta
 
-# credentials from https://apps.twitter.com/
-consumerKey = "vFutoJoPIDaF8O3U3ifJUnrFw"
-consumerSecret = "Gfy8ykTjnHf7gxj0kkakRWqY9gWhDEd69aHDWewAWP2hsxCv0z"
-accessToken = "855588527039422465-BirmEI78Vvugvm44y5cJzNoMjPiF8B8"
-accessTokenSecret = "RjZhI9lCoptKipRcKnPV6Uu4mIa4L0CGk9FoBXJ7GJNnB"
 
-#until	optional	Returns tweets created before the given date. Date should be formatted as YYYY-MM-DD. Keep in mind that the search index has a 7-day limit. In other words, no tweets will be found for a date older than one week.
 
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessTokenSecret)
 
-api = tweepy.API(auth)
 
-searched_tweets = api.search(q="hello", count=100, since="2019-11-01", until="2019-11-02")
+def my_function(query, num):
+    # credentials from https://apps.twitter.com/
+    consumerKey = "vFutoJoPIDaF8O3U3ifJUnrFw"
+    consumerSecret = "Gfy8ykTjnHf7gxj0kkakRWqY9gWhDEd69aHDWewAWP2hsxCv0z"
+    accessToken = "855588527039422465-BirmEI78Vvugvm44y5cJzNoMjPiF8B8"
+    accessTokenSecret = "RjZhI9lCoptKipRcKnPV6Uu4mIa4L0CGk9FoBXJ7GJNnB"
 
-for result in searched_tweets:
-    print(result.text)
+    N = 1
+    date_today = datetime.now();
+    date_N_days_ago = datetime.now() - timedelta(days=N)
+
+    x = str(date_N_days_ago.strftime("%Y-%m-%d"))
+    y = str(date_today.strftime("%Y-%m-%d"))
+   # print (x + " "+  y)
+
+    # until	optional	Returns tweets created before the given date. Date should be formatted as YYYY-MM-DD. Keep in mind that the search index has a 7-day limit. In other words, no tweets will be found for a date older than one week.
+
+    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    auth.set_access_token(accessToken, accessTokenSecret)
+
+    api = tweepy.API(auth)
+    searched_tweets = api.search(q=query, count=num, since=x, until=y)
+
+    for result in searched_tweets:
+        print(result.text)
+
+def main():
+    my_function(query = "hello", num = 100);
+
+main()
 
 # username = "@twitter"
 # startDate = datetime.datetime(2019, 9, 1, 0, 0, 0)
